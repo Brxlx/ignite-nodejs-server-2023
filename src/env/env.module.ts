3a78d8227906f3d23746e8b5ad1fdae4fdbd0dcd
Bruno Santos <brcesar19@gmail.com>
2024-04-22 15:26:34 -0300

    build: save exact version of packages

--- src/env/env.module.ts
+++ src/env/env.module.ts
@@ -1,12 +1,13 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+
 import { EnvService } from './env.service';
 import { envSchema } from './envSchema';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
-      validate: (env) => envSchema.parse(env),
+      validate: env => envSchema.parse(env),
       isGlobal: true,
       cache: true,
     }),
