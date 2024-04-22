89a30a3da019ef829d5fa0de7766d7c4632e2547
Bruno Santos <brcesar19@gmail.com>
2024-04-22 16:39:47 -0300

    chore: remove jest

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,10 @@
 import { Module } from '@nestjs/common';
 
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { EnvModule } from './env/env.module';
 import { PrismaService } from './prisma/prisma.service';
 
 @Module({
   imports: [EnvModule],
-  controllers: [AppController],
-  providers: [AppService, PrismaService],
+  providers: [PrismaService],
 })
 export class AppModule {}
