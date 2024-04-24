b936e4df370859c8e4f79ebd0fcfb5ce45bcb063
Bruno Santos <brcesar19@gmail.com>
2024-04-24 14:37:38 -0300

    feat(question): add migration; finish repository and mapper

--- src/infra/database/prisma/mappers/prisma-question.mapper.ts
+++ src/infra/database/prisma/mappers/prisma-question.mapper.ts
@@ -1,4 +1,4 @@
-import { Question as PrismaQuestion } from '@prisma/client';
+import { Prisma, Question as PrismaQuestion } from '@prisma/client';
 
 import { UniqueEntityID } from '@/core/entities/unique-entity-id';
 import { Question } from '@/domain/forum/enterprise/entities/question';
@@ -11,12 +11,25 @@ export class PrismaQuestionMapper {
         title: raw.title,
         content: raw.content,
         authorId: new UniqueEntityID(raw.authorId),
-        bestAnswerId: undefined,
+        bestAnswerId: raw.bestAnswerId ? new UniqueEntityID(raw.bestAnswerId) : null,
         slug: Slug.create(raw.slug),
         createdAt: raw.createdAt,
         updatedAt: raw.updatedAt,
       },
       new UniqueEntityID(raw.id)
     );
+  }
+
+  static toPrisma(question: Question): Prisma.QuestionUncheckedCreateInput {
+    return {
+      id: question.id.toString(),
+      authorId: question.authorId.toValue(),
+      bestAnswerId: question.bestAnswerId?.toString(),
+      title: question.title,
+      content: question.content,
+      slug: question.slug.value,
+      createdAt: question.createdAt,
+      updatedAt: question.updatedAt,
+    };
   }
 }
